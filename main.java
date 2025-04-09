b8c012674dee8091246066a4f236fc0fb10507eb
Ranjitrevanakar02 <Ranjitrevanakar02@gmail.com>
2025-04-09 14:26:15 +0530

    master.java

--- main.java
+++ main.java
@@ -1,10 +1,10 @@
 class Main {
     public static void main(String[] args) {
 
-      int first = 10;
-      int second = 20;
-
-      int sum = first + second;
-      System.out.println(first + " + " + second + " = "  + sum);
+        int first = 10;
+        int second = 20;
+        int t = 34;
+        int sum = first + second + t;
+        System.out.println(first + " + " + second + "" + t + " = " + sum);
     }
-  }
+}
